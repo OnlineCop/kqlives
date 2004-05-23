e18b1a4601263e5dcf073750f0bca9168247e5e4
teamterradactyl <teamterradactyl@838d8ee2-e217-4d3a-bd8c-f7f0259e51e6>
2004-05-23 16:59:24 +0000

    Updated reformat to be called for /kq path instead of /kq/bin

--- KQ/bin/Reformat.bat
+++ KQ/bin/Reformat.bat
@@ -1,5 +1,6 @@
 rem Indent all KQ files as we like them. Requires Gnu Indent
 rem See http://gnuwin32.sourceforge.net/
+rem Put indent (or indent.exe) somewhere in your search path
 
-set INDENT_PROFILE=..\other\indent.pro
-"c:\program files\gnuwin32\bin\indent" ..\src\*.c ..\include\*.h
+set INDENT_PROFILE=other\indent.pro
+indent src\*.c include\*.h
