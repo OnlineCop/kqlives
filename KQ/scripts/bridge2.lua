728dae3725ede2c6801338c8837e6f7461f65538
teamterradactyl <teamterradactyl@838d8ee2-e217-4d3a-bd8c-f7f0259e51e6>
2004-07-25 16:31:53 +0000

    New bridge initial upload

---
+++ KQ/scripts/bridge2.lua
@@ -0,0 +1,125 @@
+-- bridge2 - "On Brayden river Randen and Andra, complete"
+
+-- /*
+-- {
+-- Which globals should we have for the (incomplete) bridge?
+--
+-- P_FIGHTONBRIDGE
+--   (0-4) [Not calculated]: when this is <=4, we will not use bridge2
+--   (5) Slept at the inn, bridge is pretty close to being done
+--   (6) [Not calculated]: when this is >= 6, we will not even enter the map
+--
+-- P_LOSERONBRIDGE
+--   (0) Have not spoken to man who forgot his sword
+--   (1) Spoke to him after defeating the monsters
+--
+-- P_ASLEEPONBRIDGE
+--   (0) Have not spoken to man sleeping on bridge
+--   (1) Man is asleep again
+-- }
+-- */
+
+
+function autoexec()
+  refresh()
+end
+
+
+function refresh()
+  if (get_treasure(8) == 1) then
+    set_obs(19, 27, 0)
+    set_zone(19, 27, 0)
+  end
+
+  if (get_treasure(90) == 1) then
+    set_ftile(55, 12, 0)
+    set_zone(55, 12, 0)
+  end
+end
+
+
+function postexec()
+  return
+end
+
+
+function zone_handler(zn)
+  if (zn == 1) then
+    change_map("main", 240, 29, 240, 29)
+
+  elseif (zn == 2) then
+    -- // TT: This is still here incase the player didn't get it on the first
+    -- //  (incomplete) bridge.  They can only get it once.
+    chest(8, I_OSEED, 2)
+    autoexec()
+
+  elseif (zn == 3) then
+    chest(90, I_REGENERATOR, 1)
+    refresh()
+
+  elseif (zn == 4) then
+    change_map("main", 242, 29, 242, 29)
+
+  end
+end
+
+
+function entity_handler(en)
+  if (en == 0) then
+    bubble(en, "There have been no further threats. Thank you.")
+
+  elseif (en == 1) then
+    if (get_progress(P_LOSERONBRIDGE) == 0) then
+      bubble(en, "It's a good thing you helped us out with that monster!")
+      bubble(HERO1, "Oh, it was nothing...")
+      bubble(en, "No, really! I don't have a sword!")
+      wait(50)
+      bubble(en, "I probably shouldn't have told you that.")
+      set_progress(P_LOSERONBRIDGE, 1)
+    elseif (get_progress(P_LOSERONBRIDGE) == 1) then
+      bubble(HERO1, "So what happened to your sword?")
+      bubble(en, "...")
+      wait(50)
+      bubble(en, "My dog ate it.")
+      set_progress(P_LOSERONBRIDGE, 2)
+    elseif (get_progress(P_LOSERONBRIDGE) == 2) then
+      bubble(en, "Hey, look! I found a board with a nail in it!")
+      thought(HERO1, "Oh, good grief!")
+      bubble(en, "Fear my wrath! Hi-yah!")
+    end
+
+  elseif (en == 2) then
+    bubble(en, "Just a few more planks and this will be finished!")
+
+  elseif (en == 3) then
+    bubble(en, "I... zzz... can't stay awak... zzz...")
+
+  elseif (en == 4) then
+    bubble(en, "*YAWN!* That other guy's yawning is contagious!")
+
+  elseif (en == 5) then
+    bubble(en, "My breaks are too short. I could really use another one.")
+
+  elseif (en == 6) then
+    bubble(en, "I've planted flowers underneath the bridge.")
+
+  elseif (en == 7) then
+    bubble(en, "I'm an architect. I'm building these pillars to reinforce the bridge.")
+
+  elseif (en == 8) then
+    if (get_progress(P_BANGTHUMB) == 0) then
+      bubble(en, "Yes, wha...")
+      msg("WHAM!", 255, 0)
+      set_ent_facing(en, 2)
+      bubble(en, "Yow!! My thumb! I banged my thumb!")
+      set_ent_facing(en, 0)
+      bubble(en, "I hope that you're satisfied!")
+    else
+      bubble(en, "Owww, my poor thumb...")
+    end
+
+  elseif (en == 9) then
+    bubble(en, "Some moron rode a wagon over here with STONE RIMS! I'd like to find the no-good, lousy...")
+
+  end
+end
