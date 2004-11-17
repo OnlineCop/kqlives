ca25c15c94ee7fad00cf4766b4ee37c055b4aa6a
teamterradactyl <teamterradactyl@838d8ee2-e217-4d3a-bd8c-f7f0259e51e6>
2004-11-17 06:28:28 +0000

    Included mapdiff in compile

--- KQ/maps/make.bat
+++ KQ/maps/make.bat
@@ -6,5 +6,7 @@ gcc -Wall -W -c -o mapent.o mapent.c
 gcc -Wall -W -c -o mapshared.o mapshared.c
 gcc -Wall -W -c -o disk.o ../src/disk.c -I../include
 gcc -Wall -W -c -o mapdump.o mapdump.c
+gcc -Wall -W -c -o mapdiff.o mapdiff.c
 gcc mapedit.o mapfile.o mapshared.o mapent.o mapdata.o disk.o -o mapdraw.exe -mwindows -lalleg
 gcc mapdump.o mapshared.o disk.o -o mapdump.exe -lalleg
+gcc mapdiff.o disk.o -o mapdiff.exe -lalleg
