ce08d6a1167733f7961ec363e76c2a0b3ad3d1f4
peterhull90 <peterhull90@gmail.com>
2005-05-05 17:48:37 +0000

    Doxygen tag was bad

--- KQ/src/fade.c
+++ KQ/src/fade.c
@@ -50,7 +50,7 @@
  * \param   speed How fast to fade (1..64)
  * \param   from Starting palette index (0..255)
  * \param   to Ending palette index (0..255)
- * \datem   20040731 PH added check for out-of-range speed
+ * \date   20040731 PH added check for out-of-range speed
  */
 static void _fade_from_range (AL_CONST PALETTE source, AL_CONST PALETTE dest,
                               int speed, int from, int to)
