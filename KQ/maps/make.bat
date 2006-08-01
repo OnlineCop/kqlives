44dd77da71214e86d467b1b6523796c05141f739
teamterradactyl <teamterradactyl@838d8ee2-e217-4d3a-bd8c-f7f0259e51e6>
2006-08-01 18:08:48 +0000

    Added the -O2 compile flag for extra warnings

--- KQ/maps/make.bat
+++ KQ/maps/make.bat
@@ -4,17 +4,17 @@
 :SET_GPP
 @set KQ_COMPILER=g++
 :KQ_GO
-%KQ_COMPILER% -c -o mapdata.o mapdata.c
-%KQ_COMPILER% -c -o mapdiff.o mapdiff.c
-%KQ_COMPILER% -c -o mapdump.o mapdump.c
-%KQ_COMPILER% -c -o mapedit.o mapedit.c
-%KQ_COMPILER% -c -o mapent.o mapent.c
-%KQ_COMPILER% -c -o mapfile.o mapfile.c
-%KQ_COMPILER% -c -o mapshared.o mapshared.c
-%KQ_COMPILER% -c -o mapstructs.o mapstructs.c
+%KQ_COMPILER% -O2 -c -o mapdata.o mapdata.c
+%KQ_COMPILER% -O2 -c -o mapdiff.o mapdiff.c
+%KQ_COMPILER% -O2 -c -o mapdump.o mapdump.c
+%KQ_COMPILER% -O2 -c -o mapedit.o mapedit.c
+%KQ_COMPILER% -O2 -c -o mapent.o mapent.c
+%KQ_COMPILER% -O2 -c -o mapfile.o mapfile.c
+%KQ_COMPILER% -O2 -c -o mapshared.o mapshared.c
+%KQ_COMPILER% -O2 -c -o mapstructs.o mapstructs.c
 
-%KQ_COMPILER% -c -o disk.o ../src/disk.c -I../include
-%KQ_COMPILER% -c -o bounds.o ../src/bounds.c -I../include
+%KQ_COMPILER% -O2 -c -o disk.o ../src/disk.c -I../include
+%KQ_COMPILER% -O2 -c -o bounds.o ../src/bounds.c -I../include
 
 %KQ_COMPILER% mapedit.o mapfile.o mapshared.o mapent.o mapdata.o mapstructs.o bounds.o disk.o -o mapdraw.exe -mwindows -lalleg
 %KQ_COMPILER% mapdump.o mapshared.o disk.o -o mapdump.exe -lalleg
