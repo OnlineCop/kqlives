51e2de53ad8a465af3419525f099416239fcf01b
peterhull90 <peterhull90@gmail.com>
2008-01-14 21:32:36 +0000

    Added path searching as implemented by wk2 for unix and windows

--- KQ/src/osx.m
+++ KQ/src/osx.m
@@ -29,7 +29,6 @@
  */
 
 #import <Foundation/Foundation.h>
-/*#import <Foundation/NSPathUtilities.h>*/
 #include <string.h>
 
 /* I can't include kq.h here as it clashes with some stupid OS9 names like shadow and strbuf */
@@ -44,6 +43,68 @@ static NSString *user_dir;
 static NSString *game_dir;
 static BOOL init_path = NO;
 
+/*! \brief Returns the full path for this file
+ *
+ * This function first checks if the file can be found in the user's
+ * directory. If it can not, it checks the relevant game directory
+ * (data, music, lib, etc)
+ *
+ * \param base The first part of the string, assuming the file can't be
+ * found in user_dir (eg. "/usr/local/share/kq")
+ * \param subdir The second part of the string (eg. "maps")
+ * \param file The filename
+ * \returns the combined path
+ */
+static NSString* get_resource_file_path(NSString* base, NSString* subdir, NSString* file) {
+  NSFileManager* fm = [NSFileManager defaultManager];
+  NSString* fullpath = [NSString pathWithComponents: [NSArray arrayWithObjects: user_dir, subdir, file, nil]];
+  if ([fm fileExistsAtPath: fullpath]) {
+    return fullpath;
+  }
+  fullpath = [NSString pathWithComponents: [NSArray arrayWithObjects: base, subdir, file, nil]];
+  if ([fm fileExistsAtPath: fullpath]) {
+    return fullpath;
+  }
+  return nil;
+}
+
+/*! \brief Returns the full path for this lua file
+ *
+ * This function first checks if the lua file can be found in the user's
+ * directory. If it can not, it checks the relavent game directory
+ * (data, music, lib, etc). For each directory, it first checks for a lob
+ * file, and then it checks for a lua file. This function is similar to
+ * get_resource_file_path, but takes special considerations for lua files.
+ * Whereas get_resource_file_path takes the full filename (eg. "main.map"),
+ * this function takes the filename without extension (eg "main").
+ *
+ * \param base The first part of the string, assuming the file can't be
+ * found in user_dir (eg. "/usr/local/lib/kq")
+ * \param file The filename
+ * \returns the combined path
+ */
+NSString* get_lua_file_path(NSString* base, NSString* file) {
+  NSFileManager* fm = [NSFileManager defaultManager];
+  NSString* path = [NSString pathWithComponents: [NSArray arrayWithObjects: user_dir, @"scripts", file, nil]];
+  NSString* fullpath = [path stringByAppendingPathExtension: @"lua"];
+  if ([fm fileExistsAtPath: fullpath]) {
+    return fullpath;
+  }
+  fullpath = [path stringByAppendingPathExtension: @"lob"];
+  if ([fm fileExistsAtPath: fullpath]) {
+    return fullpath;
+  }
+  path = [NSString pathWithComponents: [NSArray arrayWithObjects: base, @"scripts", file, nil]];
+  fullpath = [path stringByAppendingPathExtension: @"lua"];
+  if ([fm fileExistsAtPath: fullpath]) {
+    return fullpath;
+  }
+  fullpath = [path stringByAppendingPathExtension: @"lob"];
+  if ([fm fileExistsAtPath: fullpath]) {
+    return fullpath;
+  }
+  return nil;
+}
 
 
 /*! \brief Return the name of 'significant' directories.
@@ -55,12 +116,12 @@ static BOOL init_path = NO;
 const char *kqres (int dir, const char *file)
 {
    static char ans[PATH_MAX];
-   NSString *base, *sub, *ff;
-   NSArray *arr;
+   NSString* found;
+   NSString* nsfile = [NSString stringWithCString: file];
    if (init_path == NO) {
       /* Get home directory */
 
-      arr = NSSearchPathForDirectoriesInDomains (NSLibraryDirectory,
+      NSArray* arr = NSSearchPathForDirectoriesInDomains (NSLibraryDirectory,
                                                  NSUserDomainMask, YES);
       user_dir =[[arr objectAtIndex: 0] stringByAppendingPathComponent:@"KQ"];
         [user_dir retain];
@@ -75,32 +136,25 @@ const char *kqres (int dir, const char *file)
 
    switch (dir) {
    case DATA_DIR:
-      base = game_dir;
-      sub = @"data";
+     found = get_resource_file_path(game_dir, @"data", nsfile);
       break;
    case MUSIC_DIR:
-      base = game_dir;
-      sub = @"music";
+     found = get_resource_file_path(game_dir, @"music", nsfile);
       break;
    case MAP_DIR:
-      base = game_dir;
-      sub = @"maps";
+     found = get_resource_file_path(game_dir, @"maps", nsfile);
       break;
    case SAVE_DIR:
    case SETTINGS_DIR:
-      base = user_dir;
-      sub = @".";
-      break;
+     found = get_resource_file_path(user_dir, @".", nsfile);
+     break;
    case SCRIPT_DIR:
-      base = game_dir;
-      sub = @"scripts";
-      break;
+     found = get_lua_file_path(game_dir, nsfile);
+     break;
    default:
-      return NULL;
+     found = nil;
+     break;
    }
-
-   ff = [NSString stringWithCString: file];
-   arr = [NSArray arrayWithObjects: base, sub,ff, nil];
-   return strncpy (ans,[[NSString pathWithComponents:arr] cString], sizeof
-                   (ans));
+   
+   return found == nil ? NULL : strncpy (ans,[found cString], sizeof(ans));
 }
